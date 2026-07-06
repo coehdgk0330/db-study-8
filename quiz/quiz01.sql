901587ad991fb86e799370ea03e327c2a2cb85a8
coehdgk0330 <coehdgk0330@gmail.com>
2026-07-06 16:46:11 +0900

    퀴즈1 기본 SELECT 쿼리 (연결연산자)

---
+++ quiz/quiz01.sql
@@ -0,0 +1,13 @@
+select 
+    name || '''s ID : '|| id 
+    || ', WEIGHT IS ' || weight || 'Kg' "ID AND WEIGHT"
+from STUDENT;
+
+select
+    ename|| '(' || job ||') , ' || ename || '''' || job || '''' "NAME AND JOB"
+    from emp;
+
+select ename || '''s sal is $' || sal "Name and Sal"
+    from emp;
+
+    
